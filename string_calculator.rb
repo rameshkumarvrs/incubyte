50223237d0051f4632920c3ddacf9912f6ec0c04
ramesh <rameshkumar.v@celestialsys.com>
2025-04-03 12:04:46 +0530

    added code logic for string number sum

--- string_calculator.rb
+++ string_calculator.rb
@@ -2,11 +2,15 @@
 class StringCalculator
   def add(str)
     return 0 if str == ""
+     str.chars.map(&:to_i).sum
   end
 end
 
 
-input_str = ""
+#input_str = ""  # input = "" o/p = 0
+ input_str = "5" # input = "5" o/p = 5
+ input_str = "1,5" # input = "1,5" o/p = 6
+
 
 s = StringCalculator.new
 s.add(input_str)
