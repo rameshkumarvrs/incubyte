e7c684a141e502f5d6675ba713035b8c4fab2392
ramesh <rameshkumar.v@celestialsys.com>
2025-04-03 13:11:57 +0530

    added code logic for add method to handle new lines between numbers

--- string_calculator.rb
+++ string_calculator.rb
@@ -6,20 +6,27 @@ class StringCalculator
      str.chars.map(&:to_i).sum
   end
 
-  # step2
+  # step2 Allow the add method to handle any amount of numbers.
   def add_multiple_numbers(str)
     str.split(',').map(&:to_i).sum
+  end
+
+  #step3 Method to handle newline between Numbers
+  def add_method_for_space_newlines(str)
+    str.split(/\s+|\n/).map(&:to_i).sum
   end
 end
 
 
-input_str = ""  # input = "" o/p = 0
-input_str = "5" # input = "5" o/p = 5
-input_str = "1,5" # input = "1,5" o/p = 6
-input_str = "10,20,30,40"
+#input_str = ""  # input = "" o/p = 0
+#input_str = "5" # input = "5" o/p = 5
+#input_str = "1,5" # input = "1,5" o/p = 6
+#input_str = "10,20,30,40,100"
+ input_str = "1\n2\n3" # input = "1\n2\n3" o/p = 6
 
 
 s = StringCalculator.new
 s.add(input_str)
-p s.add_multiple_numbers(input_str)
+s.add_multiple_numbers(input_str)
+p s.add_method_for_space_newlines(input_str)
 
