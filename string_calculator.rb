f2a8e79502b459ded9cacaf1c5e62edba4ca5438
ramesh <rameshkumar.v@celestialsys.com>
2025-04-03 11:31:57 +0530

    added logic for the emptystring as input

---
+++ string_calculator.rb
@@ -0,0 +1,13 @@
+# add method for the string calculator
+class StringCalculator
+  def add(str)
+    return 0 if str == ""
+  end
+end
+
+
+input_str = ""
+
+s = StringCalculator.new
+s.add(input_str)
+
